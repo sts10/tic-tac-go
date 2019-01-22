497681f84e064c8c71788f56dee1184df1ac9b14
Chris Piccaro <41354343+chrispiccaro18@users.noreply.github.com>
2019-01-22 15:34:59 -0800

    Occupied space check (#4)
    Check for occupied spaces

--- game.go
+++ game.go
@@ -49,6 +49,26 @@ func askForPlay() int{
 }
 
 func executePlayerMove(moveInt int, player int, b [9]int) [9]int {
+
+  // Check for occupied spaces
+  legalMove := true
+  for i := 0; i < 9; i++ {
+    if b [i] != 0 && moveInt == i {
+      fmt.Println("Please pick an unoccupied space.")
+      legalMove = false
+    }
+  } 
+  switch legalMove {
+  case true:
+    if player == 1{
+      b[moveInt] = 1
+    }else if player == 2{
+      b[moveInt] = 10
+    }
+  case false:
+    moveInt = askForPlay()
+    b = executePlayerMove(moveInt, player, b)
+
   // Check for out-of-bounds
   for moveInt > 9 {
       fmt.Println("Please enter a number under 10.")
